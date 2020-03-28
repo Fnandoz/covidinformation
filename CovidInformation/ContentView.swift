67ef3a56fb93bc286cfa9e36bf5a0f725f1ecd47
Fernando Gomes <lfgsnando@gmail.com>
2020-03-28 17:47:00 -0300

    Initial Commit

---
+++ CovidInformation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CovidInformation
+//
+//  Created by Fernando Gomes on 28/03/20.
+//  Copyright © 2020 Fernando Gomes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
